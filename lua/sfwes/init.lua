5f318e5b97fc1b774bbf6056c59e9f73734ee130
wspringe <wspringe@outlook.com>
2025-04-11 00:23:59 -0700

    feat: highlight errors in buffer

--- lua/sfwes/init.lua
+++ lua/sfwes/init.lua
@@ -12,28 +12,44 @@ function M.setup(opts)
 		end
 	end)
 
-	local stdout = ""
 	vim.api.nvim_create_autocmd("BufWritePost", {
 		callback = function()
 			if vim.bo.ft == "apex" then
+				local ns = vim.api.nvim_create_namespace("sfwes")
+				vim.diagnostic.reset(ns, 0)
 				local spinner = require("sfwes.indicator")
 				spinner.start()
-				local job = vim.fn.jobstart(sf .. " project deploy start --json", {
-					on_stdout = function(_, data, name)
-						for _, line in ipairs(data) do
-							stdout = stdout .. line .. "\n"
-						end
-					end,
-					on_stderr = function(_, data, name) end,
-					on_exit = function(_, data, exit_code)
-						if data == 0 then
-							spinner.stop("success")
-						else
-							spinner.stop("error")
-						end
-						-- print(stdout)
-					end,
-				})
+
+				local stdout = ""
+				local job =
+					vim.fn.jobstart(sf .. string.format(" project deploy start --json -d %s", vim.fn.expand("%")), {
+						on_stdout = function(_, data)
+							for _, line in ipairs(data) do
+								stdout = stdout .. line .. "\n"
+							end
+						end,
+						on_stderr = function(_, data, name) end,
+						on_exit = function(_, data, exit_code)
+							if data == 0 then
+								spinner.stop("success")
+							else
+								spinner.stop("error")
+								local result = vim.fn.json_decode(stdout)
+								local diagnostics = {}
+								for _, obj in ipairs(result.result.details.componentFailures) do
+									table.insert(diagnostics, {
+										lnum = (obj.lineNumber - 1),
+										col = obj.columnNumber - 1,
+										message = obj.problem,
+										severity = vim.diagnostic.severity.ERROR,
+										source = "sfwes",
+									})
+								end
+								print(vim.inspect(diagnostics))
+								vim.diagnostic.set(ns, 0, diagnostics)
+							end
+						end,
+					})
 			end
 		end,
 	})
